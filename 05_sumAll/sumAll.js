0e1d24d88a6d166a9ee30bd93c919cef093052f0
Vanya_Rom <ivan@romanenko.in>
2022-06-03 11:32:23 -0400

    Make function to sum all integers within the range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    let sum = 0;
+    let LgNumber;
+    let SmNumber;
 
+    if (numOne < 0 || numTwo < 0 || typeof(numOne) != 'number' || typeof(numTwo) != 'number'){
+        return('ERROR');
+    } else if (numOne < numTwo){
+        LgNumber = numTwo;
+        SmNumber = numOne;
+    } else {
+        LgNumber = numOne;
+        SmNumber = numTwo;
+    }
+    
+    for (let i = 0; i < LgNumber; i++){
+        sum = sum + SmNumber + i;
+    }
+    return(sum);
 };
 
 // Do not edit below this line
