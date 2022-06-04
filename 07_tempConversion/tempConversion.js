73b33638cdd261e656867793066d0a4db24d473f
Vanya_Rom <ivan@romanenko.in>
2022-06-03 23:05:25 -0400

    Solve task 7 Temp Conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(num) {
+  let result = Math.round( ((num - 32) * 5/9) * 10 ) / 10;
+  return(result);
 };
 
-const ctof = function() {
-
+const ctof = function(num) {
+  let result = Math.round( ((num * 9/5) + 32) * 10 ) / 10;
+  return(result);
 };
 
 // Do not edit below this line
