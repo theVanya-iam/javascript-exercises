a8874ad135a93c25889188ddc00683c57f71b909
Vanya_Rom <ivan@romanenko.in>
2022-06-07 18:46:11 -0400

    complete Fibonacci sequence algorithm

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let prev = 1;
+       let curr = 1;
+        if (num < 0) {
+            return("OOPS");
+        } else { for (let i = 2; i < num; i++){
+            next = prev + curr;
+            prev = curr;
+            curr = next;
+        }
+        return curr;
+        }
 };
 
 // Do not edit below this line
