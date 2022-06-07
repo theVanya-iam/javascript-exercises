569a241e9e463b5c2da40d3089faecd2e03b5c74
Vanya_Rom <ivan@romanenko.in>
2022-06-07 13:50:07 -0400

    add palindromes(str) function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  // this line take in the string that has been passed in as
+  // argument, lower cases all the letters, and take out non-
+  // alphanumeric characters.
+  str = str.toLowerCase().replace(/[ .,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+  let le = str.length;
 
+  if (le === 0 || le === 1) {
+    return true; 
+  } else {
+    if (str[0] === str [le - 1]) {
+      return palindromes(str.slice(1, le - 1) );
+    } else {
+      return false;
+    };
+  }
 };
-
 // Do not edit below this line
 module.exports = palindromes;
