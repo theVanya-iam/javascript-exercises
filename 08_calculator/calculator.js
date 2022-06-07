e47615202a0f54a4cec1af2debbfd317ad9fd597
Vanya_Rom <ivan@romanenko.in>
2022-06-06 20:18:49 -0400

    solve 06 through 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,59 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  const result = num1+ num2;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  const result = num1 - num2;
+  return(result);
 };
 
-const sum = function() {
-	
+const sum = function(userArr) {
+    let result = 0;
+
+    for (let i = 0; i < userArr.length; i++){
+      if (userArr.length === 0){
+        userArr[i] = 0;
+      }
+        result = parseInt(userArr[i]) + result;
+    }
+
+    return(result);
 };
 
-const multiply = function() {
+const multiply = function(userArr) {
+  let result = 1;
 
+ for (let i = 0; i < userArr.length; i++){
+   result = userArr[i] * result;
+ }
+ return(result);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	let result = 1;
+  for (let i = 0; b > 0; i++){
+    result = a * result;
+    b = b - 1;
+  }
+  return(result);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let result = 1;
+    if (a === 0 || a === 1){
+      return(1);
+    } else { 
+        let i = 1;
+        result = a;
+        for (let i = 1; i < a; i++){
+          result = result * (a-i);
+        }
+      }
+    return(result);
 };
 
+// result = 2 * (2 - 1)
 // Do not edit below this line
 module.exports = {
   add,
